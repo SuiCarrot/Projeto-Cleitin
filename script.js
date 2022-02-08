a046aa978d7a954b6eb6acdad080c42fc1f5c55e
SuiCarrot <91708805+SuiCarrot@users.noreply.github.com>
2022-02-07 21:37:57 -0300

    Versão 1.8

--- script.js
+++ script.js
@@ -1,180 +1,231 @@
-const res = require('express/lib/response');
-const { isNum } = require('pdfjs-dist/types/src/shared/util');
+var prompt = require("prompt-sync")();
 
-var prompt = require('prompt-sync')();
-
+//////////////////////////////////////////////DECLARAÇÃO DAS FUNÇÕES
 // FUNÇÃO PARA RETARDAR A CPU
 function sleep(segundos = 1) {
-    segundos = segundos * 1000;
-    let start = new Date().getTime();
-    for (let i = 0; i < 1e7; i++) {
-        if (new Date().getTime() - start > segundos) {
-            break;
-        }
+  segundos = segundos * 1000;
+  let start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > segundos) {
+      break;
     }
+  }
 }
 
 // FUNÇÃO PARA RANDOMIZAR
 function random(minimo = 0, maximo) {
-    let max = Math.floor(maximo);
-    let min = Math.ceil(minimo);
-    let random = Math.random() * (max - min + 1) + min;
-    random = parseInt(random);
-    return random;
+  let max = Math.floor(maximo);
+  let min = Math.ceil(minimo);
+  let random = Math.random() * (max - min + 1) + min;
+  random = parseInt(random);
+  return random;
+}
+
+//FUNÇÃO DE VALIDAÇÃO DE STRINGS
+function validacaoString(resposta, a, b, c, d) {
+  //Função de validação para prompt numérico
+  while (true) {
+    if (resposta == a || resposta == b || resposta == c || resposta == d) break;
+    else {
+      console.log(`Escolha uma das opçòes: `);
+      resposta = prompt(``).toUpperCase().replace(/\s/g, "");
+    }
+  }
+  return (resp = resposta);
 }
 
 // FUNÇÃO PARA CRIAR MONSTRO
 function clearMonstro() {
-    monstros.splice(0, monstros.length);
+  monstros.splice(0);
 }
 function criarMonstro(qtd, a, b) {
-    clearMonstro();
-    const listNomeMonstro = [
-        'Bahamut',
-        'Gárgula',
-        'Besta-fera',
-        'Demônio Ceifador',
-        'Batata Assassina',
-        'Assassino da Colher',
-    ];
+  clearMonstro();
+  const listNomeMonstro = [
+    "Bahamut",
+    "Gárgula",
+    "Besta-fera",
+    "Demônio Ceifador",
+    "Batata Assassina",
+    "Assassino da Colher",
+  ];
 
-    for (let i = 0; i < qtd; i++) {
-        let nomeMonstro = random(a, b);
-        let vidaMonstro = random(10, 5);
-        let danoMonstro = random(6, 2);
+  for (let i = 0; i < qtd; i++) {
+    let nomeMonstro = random(a, b);
+    let vidaMonstro = random(5, 10);
+    let danoMonstro = random(2, 4);
 
-        let monstro = {
-            nome: listNomeMonstro[nomeMonstro],
-            vida: vidaMonstro,
-            dano: danoMonstro,
-            id: i,
-        };
-        monstros.push(monstro);
-    }
-    return monstros;
+    let monstro = {
+      nome: listNomeMonstro[nomeMonstro],
+      vida: vidaMonstro,
+      dano: danoMonstro,
+      id: i,
+    };
+    monstros.push(monstro);
+  }
+  return monstros;
 }
 
-//FUNÇAÕ COMBATE
+//FUNÇÃO COMBATE
 function mortalKombat(a) {
-    do {
-        personagem.vida = personagem.vida - monstros[a].dano + personagem.defesa;
-        monstros[a].vida = monstros[a].vida - personagem.dano;
-    } while (personagem.vida > 0 && monstros[a].vida > 0);
+  do {
+    personagens.jogador.vida =
+      personagens.jogador.vida - monstros[a].dano + personagens.jogador.defesa;
+    monstros[a].vida = monstros[a].vida - personagens.jogador.dano;
+  } while (personagens.jogador.vida > 0 && monstros[a].vida > 0);
 
-    if (personagem.vida > 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if (personagens.jogador.vida > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-//FUNÇÃO DE PERGUNTAS
-
+//////////////////////////////////////////////DECLARAÇÃO DE VARIAVEIS E OBJETOS
+//ARRAY DE MONSTROS PARA SER USADO NA FUNÇÃO CRIARMONSTRO
 const monstros = [];
 
 // PERSONAGENS
-const personagem = {
-    nome: nomePersonagem,
+const personagens = {
+  jogador: {
     vida: 10,
     defesa: 3,
-    dano: random(6, 4),
-};
-
-const elfo = {
-    nome: 'Aerin',
+    dano: random(4, 6),
+  },
+  aerin: {
+    nome: `Aerin`,
     vida: 10,
-    defesa: 5,
-    dano: random(6, 4),
+    defesa: 3,
+    dano: random(4, 6),
+  },
 };
 
-const armas = {
-    machado: function () {
-        personagem.dano = personagem.dano + 3;
-        personagem.defesa = personagem.defesa - 3;
-    },
-    espadaEscudo: function () {
-        personagem.dano = personagem.dano - 1;
-        personagem.defesa = personagem.defesa + 2;
-    },
-    arco: function () {
-        personagem.dano = personagem.dano + 2;
-        personagem.defesa = personagem.defesa - 2;
-    },
+const equipamentos = {
+  machado: function () {
+    personagens.jogador.dano += 3;
+    personagens.jogador.defesa -= 3;
+  },
+  espadaEscudo: function () {
+    personagens.jogador.dano -= 1;
+    personagens.jogador.defesa += 2;
+  },
+  arco: function () {
+    personagens.jogador.dano += 2;
+    personagens.jogador.defesa -= 2;
+  },
 };
 
-let play = false;
-let nomePersonagem;
+let play = true;
 let resp;
-let quebra = true;
+let quebra = false;
+a = 0;
 
 //SCRIPT
 do {
-    //INICIO DO GAME NA CAVERNA
+  //jogar novamente
+  //INICIO DO GAME NA CAVERNA
+  console.log(
+    " Personagem acorda sem memórias em uma pequena caverna. Ao analisar os arredores vê um pequeno acampamento montado. Uma fogueira, agora apenas em brasas, com comida, uma mochila e algumas equipamentos espalhadas pelo acampamento."
+  );
+  //LAÇO PARA TRAZER OPÇÕES DA CAVERNA
+  do {
+    //personagem na caverna
     console.log(
-        ' Personagem acorda sem memórias em uma pequena caverna. Ao analisar os arredores vê um pequeno acampamento montado. Uma fogueira, agora apenas em brasas, com comida, uma mochila e algumas armas espalhadas pelo acampamento.',
+      "Diga o que quer fazer: \nComida \nAbrir Mochila\nPegar uma arma\nSair da caverna "
     );
-    //LAÇO PARA TRAZER OPÇÕES DA CAVERNA
-    do {
-        console.log(
-            'Diga o que quer fazer:\n1) Comida\n2) Abrir Mochila\n3) Pegar uma arma\n4) Sair da caverna ',
-        );
-        resp = +prompt();
-        //LAÇO PARA USUÁRIO ENTREGAR O VALOR CRRETO
-        while (resp != isNum || resp < 1 || resp > 4) {
-            console.log('Valor inserido não corresponde: ');
-            resp = +prompt();
-        }
-        //LAÇO PARA SAIR DA CAVERNA
-        do {
-            //CONDIÇÃO GAME OVER
-            if (resp === 1) {
-                console.log('A comida estava envenenada e você morreu');
-                quebra = true;
-                break;
-                //CONDIÇÃO DA MOCHILA, MOMENTO IMPORTANTE DA HISTÓRIA PORÉM NÃO FAZ NADA
-            } else if (resp == 2) {
-                console.log(
-                    `Na mochila há algumas roupas e equipamentos básicos de viagem. Junto de um bilhete pedindo para o personagem encontrar na cidade de Erast. Assinado como Aerin. Personagem tomado por memorias de batalha, lembra de seu nome: ${(nomePersonagem =
-                        prompt())}`,
-                );
-                //CONDIÇÃO IPORTANTE DE SELEÇÃO DE ARMAS, MAS AINDA NÃO SAI DA CAVERNA
-            } else if (resp == 3) {
-                console.log(
-                    'Existem 3 armas dispostas:\n1) Espada escudo = + Defesa -Ataque\n2) Machado = ++Ataque -- Defesa\n3) Arco = +Ataque - Defesa',
-                );
-                resp = +prompt();
-                //LAÇO PARA USUÁRIO ENTREGAR O VALOR CRRETO
-                while (resp != isNum || resp < 1 || resp > 3) {
-                    console.log('Valor inserido não corresponde: ');
-                    resp = +prompt();
-                }
-                if (resp == 1) {
-                    armas.espadaEscudo();
-                } else if (resp == 2) {
-                    armas.machado();
-                } else {
-                    armas.arco();
-                }
-                //CONDIÇÃO PARA SAIR DA CAVERNA
-            } else if (resp == 4) {
-                console.log('Você saiu da caverna');
-                break;
-            }
-        } while (play);
-        //CONDIÇÃO DE GAME OVER
-        if (quebra == true) {
-            break;
-        }
-    } while (play);
-    //CONDIÇÃO DE GAME OVER
-    if (quebra == true) {
-        break;
+    resp = prompt().toUpperCase().replace(/\s/g, "");
+    validacaoString(
+      resp,
+      "COMIDA",
+      "ABRIRMOCHILA",
+      "PEGARUMAARMA",
+      "SAIRDACAVERNA"
+    );
+    //CONDIÇÃO GAME OVER
+    if (resp === "COMIDA") {
+      console.log("A comida estava envenenada e você morreu");
+      quebra = true;
+      break;
+      //CONDIÇÃO DA MOCHILA, MOMENTO IMPORTANTE DA HISTÓRIA PORÉM NÃO FAZ NADA
+    } else if (resp == "ABRIRMOCHILA" && a == 0) {
+      console.log(
+        `Na mochila há algumas roupas e equipamentos básicos de viagem. Junto de um bilhete pedindo para o personagens.jogador encontrar na cidade de Erast. Assinado como Aerin. personagens.jogador tomado por memorias de batalha, lembra de seu nome:`);
+        personagens.jogador.nome = prompt()
+        
+      a = 1;
+    } else if (resp == "ABRIRMOCHILA" && a == 1) {
+      console.log(
+        `Você abre a mochila novamente, e vê os mesmos itens e um papel com o seu nome: ${personagens.jogador.nome}.`
+      );
+      //CONDIÇÃO IPORTANTE DE SELEÇÃO DE equipamentos, MAS AINDA NÃO SAI DA CAVERNA
+    } else if (resp == "PEGARUMAARMA") {
+      console.log(
+        "Existem 3 equipamentos dispostas:\nEspada escudo = + Defesa -Ataque\nMachado = ++Ataque -- Defesa\nArco = +Ataque - Defesa"
+      );
+      //LAÇO PARA USUÁRIO ENTREGAR O VALOR C0RRETO
+      resp = prompt().toUpperCase().replace(/\s/g, "");
+      validacaoString(resp, "ESPADAESCUDO", "MACHADO", "ARCO");
+      personagens.jogador.defesa = 3;
+      personagens.jogador.dano = random(4, 6);
+      if (resp == "ESPADAESCUDO") {
+        equipamentos.espadaEscudo();
+      } else if (resp == "MACHADO") {
+        equipamentos.machado();
+      } else {
+        equipamentos.arco();
+      }
+      //CONDIÇÃO PARA SAIR DA CAVERNA
+    } else if (resp == "SAIRDACAVERNA") {
+      console.log("Você saiu da caverna");
+      play = false
     }
+    //CONDIÇÃO DE GAME OVER
+    if (quebra == true) break;
+  } while (play); ////// Saida da caverna
 
-    //SAINDO DA CAVERNA 2 CAMINHOS
+  //CONDIÇÃO DE GAME OVER
+if (quebra == true) break;
 
-    //CIDADE
+console.log(`Blablablabal cenario bonito, blablabalbla, lembra de dois caminhos, blablablabla, floresta ou montanhas?`)
+resp = prompt(``).toUpperCase().replace(/\s/g, "")
+validacaoString(resposta, "FLORESTA", "MONTANHAS")
+//////////////////////////////////////////////Floresta
+/* if (resp == "FLORESTA"){
+    //Viagem
+    for (i = 0; i < 3; i++) {
+        if (i == 0) {
+      } else if (i == 1) {
+        }
+   }
+
+}
+//////////////////////////////////////////////Montanhas
+if (resp == "MONTANHAS"){
+//Viagem
+ 
+ for (i = 0; i < 5; i++) {
+     if (i == 0) {
+   } else if (i == 1) {
+     }
+}
+
+}
+
+//////////////////////////////////////////////Cidade
+if (montanhas){
+
+    if()
+    else()
+    if()
+    else()
+
+}
+  
+
+  //SAINDO DA CAVERNA 2 CAMINHOS
+ */
+  //CIDADE
 } while (play);
+personagens.jogador.vida = 10;
 
 // //Viagem
 // let dias = 5;
